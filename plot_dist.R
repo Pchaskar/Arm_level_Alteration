c97779f65227133478d228238c750626b14bbc31
Prasad Chaskar <Prasad.Chaskar@unige.ch>
2019-08-22 10:21:12 +0200

    Updates plots

--- plot_dist.R
+++ plot_dist.R
@@ -28,7 +28,7 @@ library(fitdistrplus)
 #################################
 # List all files in the chr_alt_out directory
 
-chr_alt_out<-list.files(path = "./results/", pattern = NULL, all.files = FALSE,
+chr_alt_out<-list.files(path = "./results", pattern = NULL, all.files = FALSE,
                       full.names = TRUE, recursive = FALSE,
                       ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 #################################
@@ -37,10 +37,18 @@ chr_alt_out<-list.files(path = "./results/", pattern = NULL, all.files = FALSE,
 
 readFile <- function(filename) {
   df <- read.csv(filename, header = FALSE, sep = " ")
-  df <-df[,c(2,3,4,5,6,1)]
+  df <-df[,c(3,4,5,6)]
   
-  colnames(df)<-c("CHR","GAIN","AMPLI","LOSS","LOH", "Filename")
+  colnames(df)<-c("GAIN","AMPLI","LOSS","LOH")
   
+  # remove first row
+  df = df[-1,]
+  
+  # set numeric
+  df$GAIN <- as.numeric(as.character(df$GAIN))
+  df$LOSS <- as.numeric(as.character(df$LOSS))
+  
+  # set all zero to NA
   df[df == 0] <- NA
   
 #  df$CHR<-gsub("p|q", "", df$CHR)
@@ -48,204 +56,99 @@ readFile <- function(filename) {
 return(df)
 }
 ################################
-#lONG
+#Q1: 
+#Sum
 
-df1<-readFile(chr_alt_out[1])
-df2<-readFile(chr_alt_out[2])
-df3<-readFile(chr_alt_out[3])
-df4<-readFile(chr_alt_out[4])
-df5<-readFile(chr_alt_out[5])
-df6<-readFile(chr_alt_out[6])
-df7<-readFile(chr_alt_out[7])
-df8<-readFile(chr_alt_out[8])
-df9<-readFile(chr_alt_out[9])
-df10<-readFile(chr_alt_out[10])
+df1<-readFile(chr_alt_out[23])
+df2<-readFile(chr_alt_out[24])
 
 
 ggplot() + 
-  geom_density(data=df1, aes(x=GAIN, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=GAIN, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=GAIN, col='10k_1K')) +
-  geom_density(data=df4, aes(x=GAIN, col='10k_300k')) +
-  geom_density(data=df5, aes(x=GAIN, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=GAIN, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=GAIN, col='300k_300K')) +
-  geom_density(data=df8, aes(x=GAIN, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=GAIN, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=GAIN, col='3Mb_300K'))
+  geom_density(data=df1, aes(x=GAIN, col='0k_0k')) + 
+  geom_density(data=df2, aes(x=GAIN, col='10k_0k'))
   
+ggplot() + 
+  geom_density(data=df1, aes(x=LOSS, col='0k_0k')) + 
+  geom_density(data=df2, aes(x=LOSS, col='10k_0k'))  
   
+##################################
+#Q2:
+#Sum
+
+df1<-readFile(chr_alt_out[23])
+df2<-readFile(chr_alt_out[24])
+df3<-readFile(chr_alt_out[25])
+df4<-readFile(chr_alt_out[26])
+df5<-readFile(chr_alt_out[27])
+df6<-readFile(chr_alt_out[28])
+df7<-readFile(chr_alt_out[29])
+df8<-readFile(chr_alt_out[30])
+df9<-readFile(chr_alt_out[31])
+df10<-readFile(chr_alt_out[32])
+df11<-readFile(chr_alt_out[33])
+
+#GAIN
+ggplot() + 
+  geom_density(data=df8, aes(x=GAIN, col='300k_300k'))+
+  geom_density(data=df6, aes(x=GAIN, col='300k_1M'))+
+  geom_density(data=df7, aes(x=GAIN, col='300k_10M'))
+
+
+#LOSS
+ggplot() + 
+  geom_density(data=df8, aes(x=LOSS, col='300k_300k'))+
+  geom_density(data=df6, aes(x=LOSS, col='300k_1M'))+
+  geom_density(data=df7, aes(x=LOSS, col='300k_10M'))
   
+##############################################  
+# (graph/statistic?) stating that there is no arm-bias. distribution (gains) for each arm
+seg_0k_0K<-read.csv(chr_alt_out[17], header = FALSE, sep = " ")
 
-ggplot() + 
-  geom_density(data=df1, aes(x=LOSS, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=LOSS, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=LOSS, col='10k_1K')) +
-  geom_density(data=df4, aes(x=LOSS, col='10k_300k')) +
-  geom_density(data=df5, aes(x=LOSS, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=LOSS, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=LOSS, col='300k_300K')) +
-  geom_density(data=df8, aes(x=LOSS, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=LOSS, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=LOSS, col='3Mb_300K'))
+# remove first row and first column
+seg_0k_0K<-seg_0k_0K[-1,-1]
 
+# change columns to numeric
+cols.num <- c("V3", "V4", "V5", "V6")
+seg_0k_0K[cols.num] <- sapply(seg_0k_0K[cols.num],as.character)
+seg_0k_0K[cols.num] <- sapply(seg_0k_0K[cols.num],as.numeric)
 
-################################
-#SEG
-#lONG
+# Remove amplifications from gains
+seg_0k_0K$V3<-seg_0k_0K$V3-seg_0k_0K$V4
 
-df1<-readFile(chr_alt_out[11])
-df2<-readFile(chr_alt_out[12])
-df3<-readFile(chr_alt_out[13])
-df4<-readFile(chr_alt_out[14])
-df5<-readFile(chr_alt_out[15])
-df6<-readFile(chr_alt_out[16])
-df7<-readFile(chr_alt_out[17])
-df8<-readFile(chr_alt_out[18])
-df9<-readFile(chr_alt_out[19])
-df10<-readFile(chr_alt_out[20])
+colnames(seg_0k_0K)<-c("Chr", "Gain", "Ampli", "Loss", "LOH")
+
+# Change order of chromosomes
+chrOrder<-c(paste(1:12,"p",sep=""), paste(16:21,"p",sep=""),"Xp","Yp",paste(1:22,"q",sep=""),"Xq","Yq")
+chrOrder
+
+seg_0k_0K$Chr<-factor(seg_0k_0K$Chr, levels=chrOrder)
 
 
-ggplot() + 
-  geom_density(data=df1, aes(x=GAIN, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=GAIN, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=GAIN, col='10k_1K')) +
-  geom_density(data=df4, aes(x=GAIN, col='10k_300k')) +
-  geom_density(data=df5, aes(x=GAIN, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=GAIN, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=GAIN, col='300k_300K')) +
-  geom_density(data=df8, aes(x=GAIN, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=GAIN, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=GAIN, col='3Mb_300K'))
+# Bar plot
+library(ggplot2)
+library(reshape2)
 
+# Met data frame for barchart
+seg_0k_0K_melt<-melt(seg_0k_0K, id.vars="Chr")
 
+# Plot
 
+# legend , labels theme
+My_Theme = theme(
+  axis.title.x = element_text(size = 12),
+  axis.text.x = element_text(size = 6),
+  axis.title.y = element_text(size = 12))
 
-ggplot() + 
-  geom_density(data=df1, aes(x=LOSS, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=LOSS, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=LOSS, col='10k_1K')) +
-  geom_density(data=df4, aes(x=LOSS, col='10k_300k')) +
-  geom_density(data=df5, aes(x=LOSS, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=LOSS, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=LOSS, col='300k_300K')) +
-  geom_density(data=df8, aes(x=LOSS, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=LOSS, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=LOSS, col='3Mb_300K'))
+#Rename columns
+colnames(seg_0k_0K_melt)<-c("Chromosomes", "Alterations", "Segments")
 
-##########################
-#SUM
-#lONG
+bar1 <- ggplot(data=seg_0k_0K_melt, aes(x=Chromosomes, y=Segments, fill=Alterations))
 
-df1<-readFile(chr_alt_out[21])
-df2<-readFile(chr_alt_out[22])
-df3<-readFile(chr_alt_out[23])
-df4<-readFile(chr_alt_out[24])
-df5<-readFile(chr_alt_out[25])
-df6<-readFile(chr_alt_out[26])
-df7<-readFile(chr_alt_out[27])
-df8<-readFile(chr_alt_out[28])
-df9<-readFile(chr_alt_out[29])
-df10<-readFile(chr_alt_out[30])
+bar1 + geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual(values=c("orange", "blue", "darkgreen", "purple"),
+                    name="Alterations",
+                    breaks=c("Gain", "Ampli", "Loss", "LOH"),
+                    labels=c("Gain", "Ampli", "Loss", "LOH")) + My_Theme
 
+ggsave("Arm_level_dist_alterations.pdf", width = 20, height = 15, units = "cm", dpi = 300)
 
-ggplot() + 
-  geom_density(data=df1, aes(x=GAIN, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=GAIN, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=GAIN, col='10k_1K')) +
-  geom_density(data=df4, aes(x=GAIN, col='10k_300k')) +
-  geom_density(data=df5, aes(x=GAIN, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=GAIN, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=GAIN, col='300k_300K')) +
-  geom_density(data=df8, aes(x=GAIN, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=GAIN, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=GAIN, col='3Mb_300K'))
-
-
-
-
-ggplot() + 
-  geom_density(data=df1, aes(x=LOSS, col='10k_1MB')) + 
-  geom_density(data=df2, aes(x=LOSS, col='10k_10Mb')) +
-  geom_density(data=df3, aes(x=LOSS, col='10k_1K')) +
-  geom_density(data=df4, aes(x=LOSS, col='10k_300k')) +
-  geom_density(data=df5, aes(x=LOSS, col='300k_1Mb')) +
-  geom_density(data=df6, aes(x=LOSS, col='300k_10Mb'))+
-  geom_density(data=df7, aes(x=LOSS, col='300k_300K')) +
-  geom_density(data=df8, aes(x=LOSS, col='3Mb_1Mb')) +
-  geom_density(data=df9, aes(x=LOSS, col='3Mb_10Mb')) +
-  geom_density(data=df10, aes(x=LOSS, col='3Mb_300K'))
-
-
-
-
-#f <- fitdist(data$GAIN/100, 'beta', method='mme', start=list(shape1=0.001, shape2=0.001))
-#f$estimate
-#ratio=log(f$estimate[1]/f$estimate[2])
-#ratio
-
-############################################################
-#################################
-# List all files in the chr_alt_out directory
-
-results_10k_300k<-list.files(path = "./tmp", pattern = NULL, all.files = FALSE,
-                        full.names = TRUE, recursive = FALSE,
-                        ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-
-long<-readFile(chr_alt_out[4])
-sum<-readFile(chr_alt_out[24])
-seg<-readFile(chr_alt_out[14])
-
-chr_table$length<-chr_table$Arm_end-chr_table$Arm_str
-
-
-# Plot number of segmets vs %alteration based on sum
-boxplot(split(sum$GAIN,seg$GAIN), main="Gain: trim 10k, smooth 300k ",
-        xlab="Number of segments gained", ylab="Percentage Gain")
-
-plot(sum$GAIN,seg$GAIN,xlab="Percentage Gain", ylab="Number of segments gained")
-
-boxplot(split(sum$LOSS,seg$LOSS), main="LOSS: trim 10k, smooth 300k ",
-        xlab="Number of segments lost", ylab="Percentage Alteration")
-
-plot(sum$LOSS,seg$LOSS,xlab="Percentage LOSS", ylab="Number of segments Lost")
-
-
-# Normalize the seg lengths by arm length
-for(i in 1:nrow(chr_table))
-{
-  for(j in 1:nrow(seg))
-  {
-    if (chr_table$Names[i]==seg$CHR[j])
-    {
-      seg$GAIN_norm[j]=seg$GAIN[j]/chr_table$length[i]
-      seg$LOSS_norm[j]=seg$LOSS[j]/chr_table$length[i]
-    }
-  }
-}
-
-boxplot(split(sum$GAIN,seg$GAIN_norm), main="Gain: trim 10k, smooth 300k ",
-        xlab="Number of segments", ylab="Percentage Alteration")
-
-boxplot(split(sum$LOSS,seg$LOSS_norm), main="LOSS: trim 10k, smooth 300k ",
-        xlab="Number of segments", ylab="Percentage Alteration")
-
-plot(sum$GAIN,seg$GAIN_norm,xlab="Percentage Alteration", ylab="Number of segments")
-
-plot(sum$LOSS,seg$LOSS_norm,xlab="Percentage Alteration", ylab="Number of segments")
-
-#######################3
-# Correlation sum vs long
-x_g<-sum$GAIN
-y_g<-long$GAIN
-
-cor(x_g, y_g, use = "complete.obs")
-
-plot(sum$GAIN,long$GAIN,xlab="Percentage GAIN (SUM)", ylab="Percentage GAIN (Longest)")
-
-x_l<-sum$LOSS
-y_l<-long$LOSS
-
-cor(x_l, y_l, use = "complete.obs")
-
-plot(sum$LOSS,long$LOSS,xlab="Percentage LOSS (SUM)", ylab="Percentage LOSS (Longest)")
